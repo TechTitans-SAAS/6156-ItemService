93712fd7b76f389c2baa681d69c6e03e7c5ac5b8
Meixuan Lu <meixvan.lu@mail.utoronto.ca>
2023-12-22 11:28:52 -0500

    test

--- Items/__init__.py
+++ Items/__init__.py
@@ -4,7 +4,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
 # app.config["SECRET_KEY"] = "somekey"
 app.config["MONGO_URI"] = "mongodb+srv://thriftUStore:dbUserPassword@cluster0.xzhgoim.mongodb.net/ThriftUStore?retryWrites=true&w=majority"
 
